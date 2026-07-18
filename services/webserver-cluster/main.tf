a1499ce11966252d886ddf7bdec16a1e48c20520
vishal taware <vtaware46@gmail.com>
2026-07-18 19:23:18 +0530

    commenting terraform_remote_state to check the error

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -79,16 +79,16 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
+# data "terraform_remote_state" "db" {
+#   backend = "s3"
 
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "ap-south-1"
-  }
+#   config = {
+#     bucket = var.db_remote_state_bucket
+#     key    = var.db_remote_state_key
+#     region = "ap-south-1"
+#   }
 
-}
+# }
 
 resource "aws_lb" "example" {
 
